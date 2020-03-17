91191cc845b873010d30445c27bd6fafb61317f2
John Schruben <51218424+StJohnSchruben@users.noreply.github.com>
2020-03-17 11:13:16 -0500

    Initial commit

---
+++ SSPrototype/SSMainToolWindow.cs
@@ -0,0 +1,40 @@
+﻿//------------------------------------------------------------------------------
+// <copyright file="SSMainToolWindow.cs" company="Company">
+//     Copyright (c) Company.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+
+namespace SSPrototype
+{
+    using System;
+    using System.Runtime.InteropServices;
+    using Microsoft.VisualStudio.Shell;
+
+    /// <summary>
+    /// This class implements the tool window exposed by this package and hosts a user control.
+    /// </summary>
+    /// <remarks>
+    /// In Visual Studio tool windows are composed of a frame (implemented by the shell) and a pane,
+    /// usually implemented by the package implementer.
+    /// <para>
+    /// This class derives from the ToolWindowPane class provided from the MPF in order to use its
+    /// implementation of the IVsUIElementPane interface.
+    /// </para>
+    /// </remarks>
+    [Guid("fe16a8e5-9297-4564-91aa-bb4f1887e6d3")]
+    public class SSMainToolWindow : ToolWindowPane
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SSMainToolWindow"/> class.
+        /// </summary>
+        public SSMainToolWindow() : base(null)
+        {
+            this.Caption = "SSMainToolWindow";
+
+            // This is the user control hosted by the tool window; Note that, even if this class implements IDisposable,
+            // we are not calling Dispose on this object. This is because ToolWindowPane calls Dispose on
+            // the object returned by the Content property.
+            this.Content = new SSMainToolWindowControl();
+        }
+    }
+}
---
+++ SSScripts&Bats/ResetVSInstance.bat
@@ -0,0 +1,2 @@
+cd "c:\\Program Files (x86)\\Microsoft Visual Studio 14.0\\VSSDK\\VisualStudioIntegration\\Tools\\Bin"
+CreateExpInstance /Reset /VSInstance=14.0 /RootSuffix=Exp
